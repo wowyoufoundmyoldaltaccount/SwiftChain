4d8b7e0c292a62a7dda3e1f8de1e064af30aada0
KawaiiFX <thekawaiifx@gmail.com>
2018-01-23 17:28:32 -0500

    Converted Java methods to Swift

--- SwiftChain.playground/Contents.swift
+++ SwiftChain.playground/Contents.swift
@@ -2,6 +2,8 @@
   
 import UIKit
 import PlaygroundSupport
+import Foundation
+import Security
 
 class MyViewController : UIViewController {
     override func loadView() {
@@ -17,5 +19,26 @@ class MyViewController : UIViewController {
         self.view = view
     }
 }
+
+class block {
+    var addedString = String()
+    var hash = String()
+    init(index: Int, dateCreated: String, amountTransfered: Int, previousHash: String) {
+        addedString = "\(index)\(dateCreated)\(amountTransfered)\(previousHash)"
+        var hash = calculateHash()
+    }
+    
+    func calculateHash() -> String {
+        let hashString = "\(addedString.hashValue)"
+        return hashString
+    }
+
+}
+
+class blockChain {
+    //create genesis block
+    var chain = [block(index: 0, dateCreated: "01/23/2018", amountTransfered: 0, previousHash: "0")]
+}
+
 // Present the view controller in the Live View window
 PlaygroundPage.current.liveView = MyViewController()

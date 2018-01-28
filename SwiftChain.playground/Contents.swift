fd47f3bef8b33b069532e5efb1ecdc6f35e10041
KawaiiFX <thekawaiifx@gmail.com>
2018-01-27 21:20:40 -0500

    Finished initializing functions

--- SwiftChain.playground/Contents.swift
+++ SwiftChain.playground/Contents.swift
@@ -6,13 +6,16 @@ import Foundation
 import Security
 
 class MyViewController : UIViewController {
+    
+    var coinChain = blockChain()
+    
     override func loadView() {
         let view = UIView()
         view.backgroundColor = .white
 
         let label = UILabel()
         label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
-        label.text = "Hello World!"
+        label.text = "Most recent block: \(coinChain.getLatestBlock())"
         label.textColor = .black
         
         view.addSubview(label)
@@ -23,13 +26,23 @@ class MyViewController : UIViewController {
 class block {
     var addedString = String()
     var hash = String()
+    var previousHash = String()
+    var index = Int()
+    var dateCreated = String()
+    var amountTransfered = Int()
+    
     init(index: Int, dateCreated: String, amountTransfered: Int, previousHash: String) {
         addedString = "\(index)\(dateCreated)\(amountTransfered)\(previousHash)"
-        var hash = calculateHash()
+        self.hash = calculateHash()
+        self.previousHash = previousHash
+        self.index = index
+        self.dateCreated = dateCreated
+        self.amountTransfered = amountTransfered
     }
     
     func calculateHash() -> String {
-        let hashString = "\(addedString.hashValue)"
+        var hashString = "\(addedString.hashValue)"
+        hashString = hashString.replacingOccurrences(of: "-", with: "")
         return hashString
     }
 
@@ -37,7 +50,21 @@ class block {
 
 class blockChain {
     //create genesis block
-    var chain = [block(index: 0, dateCreated: "01/23/2018", amountTransfered: 0, previousHash: "0")]
+    var chain = [block(index: 0, dateCreated: "01/27/2018", amountTransfered: 0, previousHash: "0")]
+    
+    init() {
+        print("initialized")
+    }
+
+    func getLatestBlock() -> block {
+        return chain[Int(chain.count - 1)]
+    }
+
+    func addBlock(newBlock: block) {
+        if newBlock.previousHash == getLatestBlock().hash {
+            chain.append(newBlock)
+        }
+    }
 }
 
 // Present the view controller in the Live View window

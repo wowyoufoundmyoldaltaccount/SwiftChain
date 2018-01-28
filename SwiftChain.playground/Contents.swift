badf7be20143027f85e0d89b97404ac0852077f9
KawaiiFX <thekawaiifx@gmail.com>
2018-01-28 13:24:23 -0500

    Finalized initial UI.

--- SwiftChain.playground/Contents.swift
+++ SwiftChain.playground/Contents.swift
@@ -12,9 +12,31 @@ class MyViewController : UIViewController {
     override func loadView() {
         let view = UIView()
         view.backgroundColor = .white
-
-        let walletOverviewStatusView = CAGradientLayer()
-        walletOverviewStatusView.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width*0.5, height: UIScreen.main.bounds.height * 0.653 * 0.4)
+        
+        initUserInterface(destView: view)
+        
+        self.view = view
+    }
+    
+    func initUserInterface(destView: UIView) {
+        let walletOverviewView = UIView()
+        walletOverviewView.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width*0.5, height: UIScreen.main.bounds.height * 0.653 * 0.275)
+        walletOverviewView.backgroundColor = #colorLiteral(red: 0.3015184316, green: 1, blue: 0.3607719276, alpha: 0)
+        
+        let walletTransactionsView = UIView()
+        walletTransactionsView.frame = CGRect(x: 0, y: walletOverviewView.frame.height - 15, width: UIScreen.main.bounds.width*0.5 - 9, height: UIScreen.main.bounds.height * 0.653 - walletOverviewView.frame.height)
+        walletTransactionsView.backgroundColor = .white
+        
+        walletTransactionsView.layer.zPosition = 4
+        
+        walletTransactionsView.layer.cornerRadius = 30
+        
+        walletTransactionsView.layer.shadowColor = UIColor.black.cgColor
+        walletTransactionsView.layer.shadowOpacity = 0.05
+        walletTransactionsView.layer.shadowRadius = 15.0 //Here your control your blur
+        
+        let walletOverviewStatusLayerView = CAGradientLayer()
+        walletOverviewStatusLayerView.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width*0.5, height: UIScreen.main.bounds.height * 0.653 * 0.3)
         
         let walletTitleLabel = UILabel()
         walletTitleLabel.frame = CGRect(x: 0, y: 10, width: UIScreen.main.bounds.width*0.5, height: 50)
@@ -23,42 +45,45 @@ class MyViewController : UIViewController {
         walletTitleLabel.textAlignment = .center
         walletTitleLabel.font = UIFont(name: "Avenir-Heavy", size: 20)
         
-        let walletSubtitleLabel = UILabel()
-        walletSubtitleLabel.frame = CGRect(x: 0, y: 50, width: UIScreen.main.bounds.width*0.5, height: 20)
-        walletSubtitleLabel.text = "Wallet"
-        walletSubtitleLabel.textColor = .white
-        walletSubtitleLabel.textAlignment = .center
-        walletSubtitleLabel.font = UIFont(name: "Avenir-Medium", size: 15)
-        walletSubtitleLabel.layer.opacity = 0.6
-        
         let walletBalance = UILabel()
-        walletBalance.frame = CGRect(x: 0, y: walletOverviewStatusView.frame.height / 2, width: UIScreen.main.bounds.width*0.5, height: 50)
+        walletBalance.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width*0.5, height: walletOverviewView.frame.height)
         walletBalance.text = "0.0 SXC"
         walletBalance.textColor = .white
         walletBalance.textAlignment = .center
         walletBalance.font = UIFont(name: "Avenir-Black", size: 50)
         
         let walletBalanceLabel = UILabel()
-        walletBalanceLabel.frame = CGRect(x: 0, y: walletBalance.frame.origin.y + 50, width: UIScreen.main.bounds.width*0.5, height: 20)
+        walletBalanceLabel.frame = CGRect(x: 0, y: walletBalance.frame.origin.y + 40, width: UIScreen.main.bounds.width*0.5, height: walletOverviewView.frame.height)
         walletBalanceLabel.text = "Total Balance"
         walletBalanceLabel.textColor = .white
         walletBalanceLabel.textAlignment = .center
         walletBalanceLabel.font = UIFont(name: "Avenir-Heavy", size: 15)
         walletBalanceLabel.layer.opacity = 0.6
         
+        let transactionsTitleLabel = UILabel()
+        transactionsTitleLabel.frame = CGRect(x: 0, y: 10, width: UIScreen.main.bounds.width*0.5, height: 20)
+        transactionsTitleLabel.text = "Transactions"
+        transactionsTitleLabel.textColor = #colorLiteral(red: 0.8235294118, green: 0.8392156863, blue: 0.8509803922, alpha: 1)
+        transactionsTitleLabel.textAlignment = .center
+        transactionsTitleLabel.font = UIFont(name: "Avenir-Heavy", size: 20)
+        transactionsTitleLabel.layer.opacity = 1
+        
+        transactionsTitleLabel.layer.zPosition = 5
+        
         
         let color1 = UIColor(red:0.84, green:0.55, blue:0.91, alpha:1.0).cgColor
         let color2 = UIColor(red:0.49, green:0.38, blue:0.99, alpha:1.0).cgColor
-        walletOverviewStatusView.colors = [color1, color2]
+        walletOverviewStatusLayerView.colors = [color1, color2]
         
-        walletOverviewStatusView.locations = [0.0, 1.0]
+        walletOverviewStatusLayerView.locations = [0.0, 1.0]
         
-        view.layer.addSublayer(walletOverviewStatusView)
-        view.addSubview(walletTitleLabel)
-        view.addSubview(walletSubtitleLabel)
-        view.addSubview(walletBalanceLabel)
-        view.addSubview(walletBalance)
-        self.view = view
+        destView.layer.addSublayer(walletOverviewStatusLayerView)
+        destView.addSubview(walletOverviewView)
+        destView.addSubview(walletTransactionsView)
+        walletTransactionsView.addSubview(transactionsTitleLabel)
+        walletOverviewView.addSubview(walletTitleLabel)
+        walletOverviewView.addSubview(walletBalanceLabel)
+        walletOverviewView.addSubview(walletBalance)
     }
 }
 
